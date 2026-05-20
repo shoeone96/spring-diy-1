ff5bb24f4117cc4a179c4011b1599227a892e14f
won <dlwnddnjs96@gmail.com>
2026-05-20 17:19:13 +0900

    feat: Controller URL 빈 컨테이너 자동 매핑

--- src/main/java/com/diy/framework/web/server/HandlerMapping.java
+++ src/main/java/com/diy/framework/web/server/HandlerMapping.java
@@ -1,14 +1,27 @@
 package com.diy.framework.web.server;
 
+import com.diy.framework.web.bean.BeanContainer;
+import com.diy.framework.web.bean.annotation.Controller;
 import com.diy.framework.web.controller.AbstractController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class HandlerMapping {
-    private final Map<String, AbstractController> map;
+    private final Map<String, AbstractController> map = new HashMap<>();
 
-    public HandlerMapping(Map<String, AbstractController> map) {
-        this.map = map;
+    public HandlerMapping(BeanContainer container) {
+        for (AbstractController controller : container.getBeansOfType(AbstractController.class)) {
+            Controller annotation = controller.getClass().getAnnotation(Controller.class);
+            if (annotation == null) {
+                continue;
+            }
+            String url = annotation.value();
+            if (map.containsKey(url)) {
+                throw new IllegalStateException("URL conflict: " + url);
+            }
+            map.put(url, controller);
+        }
     }
 
     public AbstractController getController(String path) {
